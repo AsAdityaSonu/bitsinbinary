8452a6195e892c41a8ff9fc56051d83826e8bf30
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2024-10-20 00:06:18 +0530

    home page updated

--- bitsinbinary/src/Components/Home/Home.jsx
+++ bitsinbinary/src/Components/Home/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <div className="title">
+        <h1>Bits In Binary</h1>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
