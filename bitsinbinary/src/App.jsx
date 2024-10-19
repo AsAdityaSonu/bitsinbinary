f152d3c24e9d3c65bc092c04531b7820b3006f48
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2024-10-20 00:41:02 +0530

    not found page updated

--- bitsinbinary/src/App.jsx
+++ bitsinbinary/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from "./Components/Home/Home"
+import NotFound from "./Components/NotFound/NotFound"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   )
---
+++ bitsinbinary/src/Components/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './NotFound.css'
+
+function NotFound() {
+  return (
+    <div title="404">404</div>
+  )
+}
+
+export default NotFound
